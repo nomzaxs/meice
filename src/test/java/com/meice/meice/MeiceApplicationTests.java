c4f719608af482f8f7190261c3097c429fe8d2ea
nomzaxs <nomzaxs@gmail.com>
2023-12-24 14:54:40 +0900

    Initial commit

---
+++ src/test/java/com/meice/meice/MeiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meice.meice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
