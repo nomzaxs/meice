e30ead8eae1f805840ed6467cc1302541b73ce1b
nomzaxs <nomzaxs@gmail.com>
2023-12-24 16:50:03 +0900

    Change Group

--- src/main/java/com/nomzaxs/meice/MeiceApplication.java
+++ src/main/java/com/nomzaxs/meice/MeiceApplication.java
@@ -1,4 +1,4 @@
-package com.meice.meice;
+package com.nomzaxs.meice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/nomzaxs/meice/MeiceApplicationTests.java
+++ src/test/java/com/nomzaxs/meice/MeiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.meice.meice;
+package com.nomzaxs.meice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
